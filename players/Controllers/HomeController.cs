eeeada2e1e2f931840139e09bc914bbe37df377c
ORIENTSOFTWARE\truong.pham <truong.pham@Dekraauto.onmicrosoft.com>
2025-10-22 14:08:51 +0700

    add level button

--- players/Controllers/HomeController.cs
+++ players/Controllers/HomeController.cs
@@ -1,4 +1,4 @@
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using players.Models;
 
@@ -15,7 +15,49 @@ namespace players.Controllers
 
         public IActionResult Index()
         {
+            var levels = new List<List<string>>
+            {
+                new() { "A Thành", "A Trung" },
+                new() { "SaoNM", "SơnTH/Quân" },
+                new() { "Luân", "A Việt" },
+                new() { "A Dũng (Việt Software)", "A Hà / A Liêm" },
+                new() { "Công", "Quyền" },
+                new() { "Hoàng", "Hinh" },
+                new() { "A Dũng Đăng", "Dương / Long" },
+                new() { "Hưng", "Tô Sơn" }
+            };
+
+            ViewBag.Levels = levels;
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult RandomizeTeams(List<List<string>> levels)
+        {
+            var team1 = new List<string>();
+            var team2 = new List<string>();
+            var rand = new Random();
+
+            foreach (var level in levels)
+            {
+                if (level.Count < 2) continue;
+
+                if (rand.Next(2) == 0)
+                {
+                    team1.Add(level[0]);
+                    team2.Add(level[1]);
+                }
+                else
+                {
+                    team1.Add(level[1]);
+                    team2.Add(level[0]);
+                }
+            }
+
+            ViewBag.Team1 = team1;
+            ViewBag.Team2 = team2;
+            ViewBag.Levels = levels;
+            return View("Index");
         }
 
         public IActionResult Privacy()
